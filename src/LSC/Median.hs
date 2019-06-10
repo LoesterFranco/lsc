ab333943bd969f27d51635e8de79616a4fc9b1d5
Andreas Westerwick <westerwick@pconas.de>
2019-06-10 22:02:54 +0200

    median by partial sort

--- src/LSC/Median.hs
+++ src/LSC/Median.hs
@@ -2,7 +2,6 @@
 module LSC.Median where
 
 import Control.Monad.ST
-import Data.Function (on)
 import Data.Vector
 import Data.Vector.Mutable (read)
 import Data.Vector.Algorithms.Intro
@@ -12,14 +11,15 @@ import Prelude hiding (length, read)
 
 
 exactMedian :: Ord a => Vector a -> a
-exactMedian = exactMedianBy (compare `on` id)
-
+exactMedian = exactMedianBy compare
+{-# INLINE exactMedian #-}
 {-# SPECIALIZE exactMedian :: Vector Int -> Int #-}
 
 
 exactMedianBy :: (a -> a -> Ordering) -> Vector a -> a
 exactMedianBy ordering v = runST $ do
     u <- thaw v
-    sortBy ordering u
-    read u $ div (length v) 2
+    let k = length v `div` 2
+    partialSortBy ordering u (succ k)
+    read u k
 
